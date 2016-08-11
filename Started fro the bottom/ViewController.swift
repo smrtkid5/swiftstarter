18f37139b0072ca37c1d93b1feec71c3eb60a12c
Carson <Carson@Users-MacBook-Pro.local>
2016-08-11 09:54:36 -0600

    no message

--- Started fro the bottom/ViewController.swift
+++ Started fro the bottom/ViewController.swift
@@ -3,7 +3,7 @@
 //  Started fro the bottom
 //
 //  Created by Carson on 2016-08-10.
-//  Copyright © 2016 Carson. All rights reserved.
+//  Copyright © 2016 Carson. All rights reserved..
 //
 
 import UIKit
